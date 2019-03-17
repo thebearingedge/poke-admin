c16ad46d65f7de48d16f72b8c15933bfcc10b7e6
thebearingedge <contact@timdav.is>
2019-03-17 11:57:18 -0700

    Create a docker container for deployment. #1

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM node:10.15.3-alpine
+
+WORKDIR /app/
+
+RUN apk add --no-cache --virtual .build-tools \
+        python \
+        make \
+        g++
+
+COPY package.json package-lock.json /app/
+
+RUN NODE_ENV=production npm ci \
+    && apk del .build-tools
+
+COPY api/ api/
+COPY client/ client/
+COPY database/ database/
+COPY schemas/ schemas/
+COPY server/ server/
+
+CMD ["node", "-r", "esm", "server/"]
--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
+import pgBump from 'pg-bump'
 import createServer from './create-server'
 import getConnections from '../database/get-connections'
 
 ;(async () => {
-  const { knex, redis } = await getConnections()
-  const dev = process.env.NODE_ENV !== 'production'
-  const server = await createServer({ dev, knex, redis })
-  server.listen(process.env.PORT, () => {
-    // eslint-disable-next-line no-console
-    console.log('listening on port', process.env.PORT)
-  })
+  /* eslint-disable no-console */
+  try {
+    console.log('Acquiring database connections...')
+    const { knex, redis } = await getConnections()
+    console.log('Running latest migrations...')
+    await pgBump.up({
+      files: 'database/migrations/',
+      connectionVar: 'DATABASE_URL',
+      journalTable: 'schema_journal'
+    })
+    console.log('Connections established.')
+    const dev = process.env.NODE_ENV !== 'production'
+    const server = await createServer({ dev, knex, redis })
+    server.listen(process.env.PORT, () => {
+      console.log('Listening on port', process.env.PORT, '.')
+    })
+  }
+  catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
 })()
