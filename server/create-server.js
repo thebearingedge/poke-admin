d6b2129f3f88ceb3f606edb3d495707c893fd8c6
thebearingedge <contact@timdav.is>
2019-03-17 17:57:10 -0700

    Enable proxy support for express. #1

--- server/create-server.js
+++ server/create-server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import helmet from 'helmet'
 import createApi from '../api/create-api'
 import createClient from '../client/create-client'
 
 export default async function createServer({ dev, knex, redis }) {
   return express()
+    .enable('trust-proxy')
+    .use(helmet())
     .use('/api', createApi({ knex, redis }))
     .use(await createClient({ dev, redis }))
 }
